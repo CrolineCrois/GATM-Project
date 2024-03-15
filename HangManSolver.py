9529b455b498a26c23f20811eae3ef8e658c15ca
CrolineCrois <maoninii@gmail.com>
2024-03-15 15:36:58 -0700

    stuff

--- HangManSolver.py
+++ HangManSolver.py
@@ -1,2 +1,55 @@
-dictionary = open("GATM-Wordle\Dictionary.txt", "r").read()
-print(dictionary)
+from collections import Counter
+
+dictionary = open("Dictionary.txt", "r")
+listDict = dictionary.read().splitlines()
+listDict = [word.lower() for word in listDict]
+countList = Counter(listDict)
+
+print("\n----------\nWelcome to HangMan Solver!!\n")
+print("Please input the word length to start:\n----------\n")
+
+wordLength = int(input())
+
+currentWord = "".join("*" for i in range(wordLength))
+
+currentList = []
+
+for word in listDict:
+    if len(word) == wordLength + 1:
+        currentList.append(word)
+
+def Matching(knownList, word):
+    for i in range(len(knownList)):
+        if word[i] == knownList[i][1]:
+            continue
+        else:
+            return false
+    return true
+
+for i in range(26):
+    
+    if "*" not in currentWord:
+        break
+
+    letter, counts = countList.most_common(1)[0]
+
+    print("\n----------\nSuggested Guess Letter: " + letter + "\n")
+    print("Please input the known letters and their position in this form: hell*\n")
+
+    currentWord = input()
+
+    knownPos = []
+
+    for char in currentWord:
+        if char == "*":
+            continue
+        knownPos.append((char, currentWord.index(char.lower())))
+
+    print(knownPos)
+    break
+
+
+
+
+
+
