1186f518aaa7428415e3c6d33d5ec9483d9cc7ab
CrolineCrois <maoninii@gmail.com>
2024-03-18 16:18:55 -0700

    works now

--- HangManSolver.py
+++ HangManSolver.py
@@ -1,53 +1,78 @@
 from collections import Counter
 
-dictionary = open("Dictionary.txt", "r")
-listDict = dictionary.read().splitlines()
-listDict = [word.lower() for word in listDict]
-countList = Counter(listDict)
-
-print("\n----------\nWelcome to Snowman Solver!!\n")
-print("Please input the word length to start:\n----------\n")
-
-wordLength = int(input())
-
-currentWord = "".join("*" for i in range(wordLength))
-
-currentList = []
-
-for word in listDict:
-    if len(word) == wordLength + 1:
-        currentList.append(word)
-
 def Matching(knownList, word):
-    for i in range(len(knownList)):
-        if word[i] == knownList[i][1]:
+    for x in range(len(knownList)):
+        if word[knownList[x][1]] == knownList[x][0]:
             continue
         else:
-            return false
-    return true
+            return False
+    return True
 
-for i in range(26):
+def playHangMan():
+    dictionary = open("Dictionary.txt", "r")
+    listDict = dictionary.read().splitlines()
+    listDict = [word.lower() for word in listDict]
+    countList = Counter(listDict)
+
+    oldList = []
+    newList = []
+
+    wordsGuessed = []
+
+    print("\n----------\nWelcome to HangMan Solver!!\n")
+    print("Please input the word length to start:\n----------\n")
+
+    wordLength = int(input())
     
-    if "*" not in currentWord:
-        break
+    for word in listDict:
+        if len(word) == wordLength:
+            oldList.append(word)
 
-    letter, counts = countList.most_common(1)[0]
+    currentWord = "".join("*" for i in range(wordLength))
 
-    print("\n----------\nSuggested Guess Letter: " + letter + "\n")
-    print("Please input the known letters and their position in this form: hell*\n")
+    for i in range(26):
+        newList = []
+        
+        if "*" not in currentWord:
+            break
 
-    currentWord = input()
+        letter = GetMostCommon(oldList, wordsGuessed)
+        #(Counter("".join(oldList))).most_common(1)[0] #get rid of alr used letters
 
-    knownPos = []
+        print("\n----------\nSuggested Guess Letter: " + letter + "\n")
+        print("Please input the known letters and their position in this form: bonjo*r\n")
+        wordsGuessed.append(letter)
 
-    for char in currentWord:
-        if char == "*":
-            continue
-        knownPos.append((char, currentWord.index(char.lower())))
+        currentWord = input()
 
-    print(knownPos)
-    break
+        knownPos = []
 
+        for char in currentWord:
+            if char == "*":
+                continue
+            knownPos.append([char, currentWord.index(char.lower())])
+        
+        for word in oldList:
+            if Matching(knownPos, word):
+                newList.append(word)
+
+        if len(newList) == 1:
+            print("the answer is: " + newList[0] + "!")
+            return
+
+        oldList = newList
+
+def GetMostCommon(listt, wordsGuessed):
+    c = 1
+    currentLetter = (Counter("".join(listt))).most_common(c)[c-1][0]
+    while currentLetter in wordsGuessed:
+        c += 1
+        currentLetter = (Counter("".join(listt))).most_common(c)[c-1][0]
+        if currentLetter not in wordsGuessed:
+            break
+    return(currentLetter)
+
+playHangMan()
 
 
 
